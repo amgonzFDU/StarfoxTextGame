2f7f794549f77694ed68ace69862689906d6dde0
amgonzFDU <55163685+amgonzFDU@users.noreply.github.com>
2019-10-14 20:36:36 -0400

    Update and rename Main to Main.cpp

---
+++ CodeDesign/Main.cpp
@@ -0,0 +1,42 @@
+// The HelloWorld test program
+
+#include <iostream>
+
+int main() {
+	//Create strings for inputs from the user
+	//Create an array for commands that are acceptable (get, go, use, etc.)
+
+	//initialize rooms
+	//initialize items
+
+	// Display game rules
+
+	//Request for command
+
+	/*
+	this should check the players input and confirm if its valid.
+	While its valid it will decide what should happen next.
+	while(checkCommand(inputString)){
+		if(inputString == quit){
+		exit game
+		}
+		elseif(input contains go){
+		
+		}
+		.
+		.
+		.
+	}
+	*/
+}
+//Anthony Gonzalez
+
+/*
+boolean checkCommand(string playerCommand){
+	for(acceptable commands){
+		if(playerCommand = accetableCommand){
+		return true;
+		}
+	}
+	return false;
+}*/

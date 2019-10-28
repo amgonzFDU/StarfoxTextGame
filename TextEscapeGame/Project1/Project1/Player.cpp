14c1b03d93d759a1e1ee0c121fa6cbc11dcc70f8
noobsman <roarkeschafer@gmail.com>
2019-10-28 16:00:22 -0400

    Added Location to the player object. Added set/get location for player objects.

--- TextEscapeGame/Project1/Project1/Player.cpp
+++ TextEscapeGame/Project1/Project1/Player.cpp
@@ -4,6 +4,7 @@ Defult player constructor
 */
 Player::Player(std::string name)
 {
+	this->location = nullptr;
 	this->name = name;
 }
 
@@ -21,3 +22,20 @@ std::string Player::getName()
 {
 	return this->name;
 }
+
+/*
+Returns a pointer to the current location of the player.
+*/
+Room* Player::getCurrentRoom()
+{
+	return this->location;
+}
+
+/*
+Sets the location of the player.
+*/
+void Player::setCurrentRoom(Room& newRoom)
+{
+	this->location = &newRoom;
+}
+
--- TextEscapeGame/Project1/Project1/Player.h
+++ TextEscapeGame/Project1/Project1/Player.h
@@ -1,12 +1,16 @@
 #include <string>
+#include "Room.h"
 
 class Player
 {
 private:
 	std::string name;
+	Room* location;
 public:
 	Player(std::string name);
 	~Player();
+	Room* getCurrentRoom();
+	void setCurrentRoom(Room& newRoom);
 	std::string getName();
 };
 

862f8d5b5dbe6014b27eec25ededce5e457f0f1c
Jeremy Brendli <jbrendli@student.fdu.edu>
2019-11-12 20:14:44 -0500

    Jira issue FOX - 145

--- TextEscapeGame/Project1/Project1/Main.cpp
+++ TextEscapeGame/Project1/Project1/Main.cpp
@@ -25,163 +25,180 @@ bool validateInput(std::string userInput) {
 	}
 	return false;
 }
+std::string USERINPUT() {
+	std::string userInput;
+	std::getline(std::cin, userInput);
+	std::transform(userInput.begin(), userInput.end(), userInput.begin(), ::toupper);
 
-
+	if (std::cin.eof()) {
+		std::cin.clear();
+		return "You did not input a correct action.\n";
+	}
+	return userInput;
+}
 int main(int argc, char *argv[])
 {
 	
-	//Inializes an array that will hold the items with their properties
-	Items item[ITEMS];
-	//sets the name of the item
-	item->setItemLocation(item);
+		//Inializes an array that will hold the items with their properties
+		Items item[ITEMS];
+		//sets the name of the item
+		item->setItemLocation(item);
 
-	//Inializes an array that will hold the rooms with proprties
-	Room room[ROOMS];
-	//sets the properties of the items
-	room->setRoom(room);
-
-
+		//Inializes an array that will hold the rooms with proprties
+		Room room[ROOMS];
+		//sets the properties of the items
+		room->setRoom(room);
 
 
-	std::cout << "Please Enter your name.\n";
-	std::string name;
-    std::cin >> name;
-	Player* MainPlayer = new Player(name);
 
-	std::cout << "Hello there " + MainPlayer->getName(); + "";
-	std::cout << " you can type 'help' for a list of rules and commands.\n";
-	std::cout << "\n";
 
-	std::string userInput;
-	
-	/*
-		N
-		|
-	W---|---E
-		|
-		S
-	Test Room Layout
-	| 1 | 2 | 3 |
-	-------------
-	| 4 | 5 | 6 |
-	-------------
-	| 7 | 8 | 9 |
-	*/
-
-	
-	
-	
-	// points to current room
-	Room* CURRENTROOM = new Room(room , 1);
-	
-	//sets current room to the first room 0 which is the cell
-	CURRENTROOM->setCurrentRoom(room, 1);
-	std::cout << CURRENTROOM->getRoomDiscription(room,1) << std::endl;
-	item->outputRoomItems(item, CURRENTROOM->getCurrentRoomNumber());
-	while (userInput != "QUIT") {
-		userInput.clear();
-		std::cout << "What do you want to do?\n";
-		std::getline(std::cin,userInput);
-		std::transform(userInput.begin(), userInput.end(), userInput.begin(), ::toupper);
-		if (!validateInput(userInput)) {
-			std::cout << "Enter a valid command \n"; //not working properly
-		}		
-		else if (userInput == "QUIT") { 
-			std::cout << "Quiting \n";
-		}		
-		else{
-			std::transform(userInput.begin(), userInput.end(), userInput.begin(), ::toupper);
-			if (userInput == "NORTH")
-			{
-				int currentRoomNumber = CURRENTROOM->getCurrentRoomNumber();
-				CURRENTROOM->moveRoom(room, currentRoomNumber, 0, item);
-				//std::cout << "The room is: " << CURRENTROOM->getCurrentRoomName() << "\n";
-			}
-			else if (userInput == "SOUTH")
-			{
-				int currentRoomNumber = CURRENTROOM->getCurrentRoomNumber();
-				CURRENTROOM->moveRoom(room, currentRoomNumber, 1, item);
-				//std::cout << "The room is: " << CURRENTROOM->getCurrentRoomName() << "\n";
-			}
-			else if (userInput == "EAST")
-			{
-				int currentRoomNumber = CURRENTROOM->getCurrentRoomNumber();
-				CURRENTROOM->moveRoom(room, currentRoomNumber, 2, item);
-				//std::cout << "The room is: " << CURRENTROOM->getCurrentRoomName() << "\n";
-			}
-			else if (userInput == "WEST")
-			{
-				int currentRoomNumber = CURRENTROOM->getCurrentRoomNumber();
-				CURRENTROOM->moveRoom(room, currentRoomNumber, 3, item);
-				//std::cout << "The room is: " << CURRENTROOM->getCurrentRoomName() << "\n";
-				
-			}
-			else if (userInput == "INVENTORY") {
-
-				item->outputPlayerItems(item);
-
-			}
-			else if (userInput == "HELP") {
-				std::cout << "All you need to do is try to get out, can you make it to the exit? \n";
-				std::cout << "Type: 'North', 'South', 'East', or 'West' to move around the rooms\n";
-				std::cout << "Type: 'Room' if you need to see what room you are in.\n";
-				std::cout << "Type: 'get' to pick up items and 'drop' to drop them\n";
-			}	
-			else if (userInput == "USE") {
-				std::cout << "What would you like to use?\n";
-				
-				/*
-				userInput.clear();
-				std::cin >> Item //input item being used
-				std::cout << "What would you like to use" + Item + "on? \n";
-				std::cin >> Lock //what the item is being used on
-				// some to be made function to that inputs Item and Lock and will edit the board adn change room diolog
-				Item.clear();
-				Lock.clear();
-				*/
-				
-			}
-			else if (userInput == "THROW") {
-				std::cout << "What would you like to throw?\n";
-				
-				//will have very similar code to use referance that for details when ready.
-				
-			}
-			else if (userInput == "DROP") {
-				userInput.clear();
-				std::cout << "What Item do you want to drop?\n";
-				std::getline(std::cin,userInput);//Rock
-				std::transform(userInput.begin(), userInput.end(), userInput.begin(), ::toupper);
-				int currentRoomNumber = CURRENTROOM->getCurrentRoomNumber();
-				std::string CurrentRoomName = CURRENTROOM->getCurrentRoomName();
-				item->PlayerDrop(item,userInput, currentRoomNumber,CurrentRoomName);
-			}
-			else if (userInput == "ROOM") {
-				std::cout << "The room is: " << CURRENTROOM->getCurrentRoomName() << "\n";
-			}
-			else if (userInput == "GET")//pick up
-			{
-				userInput.clear();
-				std::cout << "What Item do you want to pick up?\n";
-				std::getline(std::cin,userInput);//Rock
-				std::transform(userInput.begin(), userInput.end(), userInput.begin(), ::toupper);
-	
-     			/*tries to find the item according to user input 
-				and return as a number in the enum	
-				we need to know the item they want to pick up
-				so if they type rock it should go in as a number*/
-				int currentRoomNumber = CURRENTROOM->getCurrentRoomNumber();
-				item->PlayerPickup(item, userInput,currentRoomNumber);
-			}
-			
+		std::cout << "Please Enter your name.\n";
+		std::string name;
+		name = USERINPUT();
+		while (name  == "You did not input a correct action.\n") {
+			std::cout << "You are not inputing a valid charactor\n";
+			name = USERINPUT();
 		}
-		
-				
-	}
+
+		Player* MainPlayer = new Player(name);
+
+		std::cout << "Hello there " + MainPlayer->getName(); +"";
+		std::cout << " you can type 'help' for a list of rules and commands.\n";
+		std::cout << "\n";
+
+		std::string userInput;
+
+		/*
+			N
+			|
+		W---|---E
+			|
+			S
+		Test Room Layout
+		| 1 | 2 | 3 |
+		-------------
+		| 4 | 5 | 6 |
+		-------------
+		| 7 | 8 | 9 |
+		*/
+
+
+
+
+		// points to current room
+		Room* CURRENTROOM = new Room(room, 1);
+
+		//sets current room to the first room 0 which is the cell
+		CURRENTROOM->setCurrentRoom(room, 1);
+		std::cout << CURRENTROOM->getRoomDiscription(room, 1) << std::endl;
+		item->outputRoomItems(item, CURRENTROOM->getCurrentRoomNumber());
+		while (userInput != "QUIT") {
+			userInput.clear();
+			std::cout << "What do you want to do?\n";
+			userInput = USERINPUT();
+			
+			
+			if (!validateInput(userInput)) {
+				std::cout << "Enter a valid command \n"; //not working properly
+			}
+			else if (userInput == "QUIT") {
+				std::cout << "Quiting \n";
+			}
+			else {
+				std::transform(userInput.begin(), userInput.end(), userInput.begin(), ::toupper);
+				if (userInput == "NORTH")
+				{
+					int currentRoomNumber = CURRENTROOM->getCurrentRoomNumber();
+					CURRENTROOM->moveRoom(room, currentRoomNumber, 0, item);
+					//std::cout << "The room is: " << CURRENTROOM->getCurrentRoomName() << "\n";
+				}
+				else if (userInput == "SOUTH")
+				{
+					int currentRoomNumber = CURRENTROOM->getCurrentRoomNumber();
+					CURRENTROOM->moveRoom(room, currentRoomNumber, 1, item);
+					//std::cout << "The room is: " << CURRENTROOM->getCurrentRoomName() << "\n";
+				}
+				else if (userInput == "EAST")
+				{
+					int currentRoomNumber = CURRENTROOM->getCurrentRoomNumber();
+					CURRENTROOM->moveRoom(room, currentRoomNumber, 2, item);
+					//std::cout << "The room is: " << CURRENTROOM->getCurrentRoomName() << "\n";
+				}
+				else if (userInput == "WEST")
+				{
+					int currentRoomNumber = CURRENTROOM->getCurrentRoomNumber();
+					CURRENTROOM->moveRoom(room, currentRoomNumber, 3, item);
+					//std::cout << "The room is: " << CURRENTROOM->getCurrentRoomName() << "\n";
+
+				}
+				else if (userInput == "INVENTORY") {
+
+					item->outputPlayerItems(item);
+
+				}
+				else if (userInput == "HELP") {
+					std::cout << "All you need to do is try to get out, can you make it to the exit? \n";
+					std::cout << "Type: 'North', 'South', 'East', or 'West' to move around the rooms\n";
+					std::cout << "Type: 'Room' if you need to see what room you are in.\n";
+					std::cout << "Type: 'get' to pick up items and 'drop' to drop them\n";
+				}
+				else if (userInput == "USE") {
+					std::cout << "What would you like to use?\n";
+
+					/*
+					userInput.clear();
+					std::cin >> Item //input item being used
+					std::cout << "What would you like to use" + Item + "on? \n";
+					std::cin >> Lock //what the item is being used on
+					// some to be made function to that inputs Item and Lock and will edit the board adn change room diolog
+					Item.clear();
+					Lock.clear();
+					*/
+
+				}
+				else if (userInput == "THROW") {
+					std::cout << "What would you like to throw?\n";
+
+					//will have very similar code to use referance that for details when ready.
+
+				}
+				else if (userInput == "DROP") {
+					userInput.clear();
+					std::cout << "What Item do you want to drop?\n";
+					userInput = USERINPUT();
+
+					int currentRoomNumber = CURRENTROOM->getCurrentRoomNumber();
+					std::string CurrentRoomName = CURRENTROOM->getCurrentRoomName();
+					item->PlayerDrop(item, userInput, currentRoomNumber, CurrentRoomName);
+				}
+				else if (userInput == "ROOM") {
+					std::cout << "The room is: " << CURRENTROOM->getCurrentRoomName() << "\n";
+				}
+				else if (userInput == "GET")//pick up
+				{
+					userInput.clear();
+					std::cout << "What Item do you want to pick up?\n";
+					
+					userInput = USERINPUT();	
+					
+					/*tries to find the item according to user input
+					and return as a number in the enum
+					we need to know the item they want to pick up
+					so if they type rock it should go in as a number*/
+					int currentRoomNumber = CURRENTROOM->getCurrentRoomNumber();
+					item->PlayerPickup(item, userInput, currentRoomNumber);
+				}
+
+			}
+
+
+		}
+
+
+
+		std::cout << "Goodbye.";
+		exit(0);
+		return 0;
 	
-	
-	
-	std::cout << "Goodbye.";
-	exit(0);
-	return 0;
 }
+
